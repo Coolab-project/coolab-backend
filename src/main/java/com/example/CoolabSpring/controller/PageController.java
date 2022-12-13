d0bdb29b7bb86d5bf7f24fe4c5149b76be11cabd
Forests <99336909+forests0@users.noreply.github.com>
2022-12-13 21:40:13 +0900

    add : User, Teams 추가

---
+++ src/main/java/com/example/CoolabSpring/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.example.CoolabSpring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class PageController {
+    @GetMapping("hello")
+    public List<String> hello() {
+        return Arrays.asList("정현우", "Hello");
+    }
+}
---
+++ src/main/java/com/example/CoolabSpring/domain/Teams.java
@@ -0,0 +1,2 @@
+package com.example.CoolabSpring.domain;public class Teams {
+}
